c5dd6e4acc03cf714e5f05a918a3c711cbe66dce
Ubuntu <azureuser@pt-vm.pk1joupuwysufmx3fnj3001dlc.bx.internal.cloudapp.net>
2023-03-29 13:37:20 +0000

    procfile added

---
+++ transcription/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+# class mp3file(models.Model):
+#     filename=models.CharField(max_length=64)
+#     url=models.CharField(max_length=64)
+#     audio_file=models.CharField(max_length=64)
+#     text=models.CharField(max_length=64)
