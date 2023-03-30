5ca056f8dcd098643e3a7772efd99ccf79191d7f
Ubuntu <azureuser@pt-vm.pk1joupuwysufmx3fnj3001dlc.bx.internal.cloudapp.net>
2023-03-30 05:15:03 +0000

    changes

--- yt/yt/settings.py
+++ yt/yt/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,9 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
